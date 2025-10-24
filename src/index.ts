83ae47e2b9180e4ed195e0092d0529c2dbb93322
aashishpanchal <aipanchal51@gmail.com>
2025-10-24 11:57:08 +0530

    feat: add types export for './types' in package.json

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,4 @@ export * from './helps';
 export {Router} from './router';
 export {handler} from './handler';
 export {HttpStatus} from './status';
-export type {
-  RType,
-  Handler,
-  HttpStatusCode,
-  ClientErrorStatusCode,
-  RedirectStatusCode,
-  ServerErrorStatusCode,
-  SuccessStatusCode,
-} from './types';
 export {errorHandler, notFound, poweredBy} from './middle';
